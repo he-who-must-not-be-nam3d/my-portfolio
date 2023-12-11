7ba1843e278a098a8b0abda8137cb05607dd59ac
he-who-must-not-be-nam3d <wanjurisamuel@gmail.com>
2023-12-11 18:36:58 +0300

    Updated
    Created directory for portfolio files

---
+++ my-portfolio/script.js
@@ -0,0 +1,13 @@
+// Example JavaScript code
+
+// Function to log a welcome message to the console
+function logWelcomeMessage() {
+    console.log("Welcome to my portfolio! Feel free to explore.");
+}
+
+// Call the function when the page has finished loading
+document.addEventListener("DOMContentLoaded", function() {
+    logWelcomeMessage();
+    
+    // You can add more JavaScript logic or interactions here
+});
